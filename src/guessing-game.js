942ac98a61f9a6c83138b8624f7adc1a46facf0f
Vil <106656745+sohodoll@users.noreply.github.com>
2022-08-11 15:25:56 +0300

    finished

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
+
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.guessnum = Math.round((this.min + this.max) / 2);
+        return this.guessnum;
     }
 
     lower() {
-
+        this.max = this.guessnum;
     }
 
     greater() {
-
+        this.min = this.guessnum;
     }
+
 }
 
 module.exports = GuessingGame;
